5e8bb37c55c75598bb5050882bdf116ef14fc3cd
web30x <kunmykel1@gmail.com>
2023-02-20 17:19:27 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints letter in reverse
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

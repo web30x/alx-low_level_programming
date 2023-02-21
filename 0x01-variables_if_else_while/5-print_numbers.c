5e6cc9c33235832aac09210ba534510bf5e62ef3
web30x <kunmykel1@gmail.com>
2023-02-20 17:16:46 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints single digits of base ten
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}

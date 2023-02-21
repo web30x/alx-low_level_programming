d7b89acbaa59039ef1811023adbd716451f0b718
web30x <kunmykel1@gmail.com>
2023-02-20 17:18:02 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints numbers using putchar
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch + '0');
+	}
+	putchar('\n');
+	return (0);
+}
